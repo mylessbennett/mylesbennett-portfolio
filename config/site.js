03d7051acb0e4cbc53d7249d73c374297356f905
Myles Bennett <myles.bennett@hotmail.com>
2019-05-06 19:32:16 -0400

    added personal info

--- config/site.js
+++ config/site.js
@@ -1,19 +1,19 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Emilia', // Navigation and Site Title
-  siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Emilia', // short_name for manifest
-  siteHeadline: 'Publishing & Creating stunning photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://emilia.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Myles Bennett | Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Myles', // Alternative Site title for SEO
+  siteTitleShort: 'Myles', // short_name for manifest
+  siteHeadline: 'Creating beautiful websites', // Headline for schema.org JSONLD
+  siteUrl: 'https://condescending-stonebraker-eac330.netlify.com/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@emilia', // Twitter Username
-  ogSiteName: 'emilia', // Facebook Site Name
+  userTwitter: '', // Twitter Username
+  ogSiteName: '', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-47519312-4',
 
@@ -22,21 +22,17 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Your information
-  avatar: '/logos/lekoarts.png',
-  name: 'LekoArts',
-  location: 'Germany',
+  avatar: '/logos/headshot.png',
+  name: 'Myles Bennett',
+  location: 'Toronto',
   socialMedia: [
     {
-      url: 'https://twitter.com/lekoarts_de',
-      name: 'Twitter',
+      url: 'https://www.linkedin.com/in/myles-bennett',
+      name: 'LinkedIn',
     },
     {
-      url: 'https://www.instagram.com/lekoarts.de',
-      name: 'Instagram',
-    },
-    {
-      url: 'https://www.lekoarts.de/en',
-      name: 'Homepage',
+      url: 'https://github.com/mylessbennett',
+      name: 'Github',
     },
   ],
 }
